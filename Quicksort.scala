a730b29e5f201be1299494e871db5f03af373237
Michael Galpin <mike.sr@gmail.com>
2012-04-02 07:00:14 -0700

    Made generic

--- Quicksort.scala
+++ Quicksort.scala
@@ -1,3 +1,5 @@
+import scala.math.Ordered
+
 object Quicksort{
     def main(args:Array[String]){
         var nums = Array(3,9,8,2,1,5,4,6,7)
@@ -5,11 +7,17 @@ object Quicksort{
         println(nums.toList)
     }
 
-    def quicksort(nums:Array[Int]){
+    def quicksort[T <% Ordered[T]](nums:Array[T]){
         quicksort(nums, 0, nums.length - 1)
     }
+
+    private def swap[T](nums:Array[T], i:Int, j:Int){
+        var temp = nums(i)
+        nums(i) = nums(j)
+        nums(j) = temp
+    }    
     
-    def quicksort(nums:Array[Int], start:Int, end:Int){
+    private def quicksort[T <% Ordered[T]](nums:Array[T], start:Int, end:Int){
         if (end - start < 1){
             // no-op
         } else if (end - start == 1){
@@ -29,15 +37,9 @@ object Quicksort{
         }
     }
 
-    def swap(nums:Array[Int], i:Int, j:Int){
-        var temp = nums(i)
-        nums(i) = nums(j)
-        nums(j) = temp
-    }
+    private def choosePivot[T](array:Array[T], start:Int, end:Int) = start
 
-    def choosePivot(array:Array[Int], start:Int, end:Int) = start
-
-    def partition(nums:Array[Int], start:Int, end:Int) = {
+    private def partition[T <% Ordered[T]](nums:Array[T], start:Int, end:Int) = {
         var pivotBoundary, partitionBoundary = start + 1
         var pivot = nums(start)
         while (partitionBoundary <= end){
