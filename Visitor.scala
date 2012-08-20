8ec6643994b823798e9cfb3750f95dd796120457
Michael Galpin <mike.sr@gmail.com>
2012-08-19 17:59:04 -0700

    Touched up for book

--- Visitor.scala
+++ Visitor.scala
@@ -1,54 +1,77 @@
 import scala.collection.mutable.ListBuffer
 
 trait Message{
-	var sender:String = null
-	def accept(visitor:MessageVisitor)
+  var sender:String = null
+  var recipient:String = null
+  var timestamp:Long = 0L
+  def accept(visitor:MessageVisitor)
 }
 
 trait MessageVisitor{
-	def visit(textMessage:TextMessage)
-	def visit(multimediaMessage:MultimediaMessage)
+  def visit(textMessage:TextMessage)
+  def visit(multimediaMessage:MultimediaMessage)
 }
 
 class TextMessage extends Message{
-	var text:String = ""
+  var text:String = ""
 
-	override def accept(visitor:MessageVisitor){
-		visitor.visit(this)
-	}
+  override def accept(visitor:MessageVisitor){
+    visitor.visit(this)
+  }
 }
 
 class MultimediaMessage extends Message{
-	var attachment:Array[Byte] = Array()
-	var mime:String = ""
+  var attachment:Array[Byte] = Array()
+  var mime:String = ""
 
-	override def accept(visitor:MessageVisitor){
-		visitor.visit(this)
-	}
+  override def accept(visitor:MessageVisitor){
+    visitor.visit(this)
+  }
 }
 
 class SizeVisitor extends MessageVisitor{
-	var size:Int = 0
+  var size:Int = 0
 
-	override def visit(textMessage:TextMessage){
-		size += textMessage.text.length
-	}
+  override def visit(textMessage:TextMessage){
+    size += textMessage.text.length
+  }
 
-	override def visit(multimediaMessage:MultimediaMessage){
-		size += multimediaMessage.attachment.length
-	}
+  override def visit(multimediaMessage:MultimediaMessage){
+    size += multimediaMessage.attachment.length
+  }
+}
+
+class SentSizeVisitor(var owner:String) extends MessageVisitor{
+  var size:Int = 0
+	
+  override def visit(textMessage:TextMessage){
+    if (owner == textMessage.sender){
+      size += textMessage.text.length
+    }
+  }
+
+  override def visit(multimediaMessage:MultimediaMessage){
+    if (owner == textMessage.sender){		
+      size += multimediaMessage.attachment.length
+    }
+  }	
 }
 
 class MessageHistory{
-	var messages:ListBuffer[Message] = ListBuffer.empty[Message]
+  var messages:ListBuffer[Message] = ListBuffer.empty[Message]
+  var owner:String = ""
+}
+def size = {
+  var sizeVisitor = new SizeVisitor
+  var i = 0
+  while (i < messages.size){
+    messages(i).accept(sizeVisitor)
+    i += 1
+  }
+  sizeVisitor.size
+}
 
-	def size = {
-		var sizeVisitor = new SizeVisitor
-		var numMessages = messages.size
-		var i = 0
-		while (i < numMessages){
-			messages(i).accept(sizeVisitor)
-		}
-		sizeVisitor.size
+	def sizeSent = {
+
 	}
 }
--- functional/Visitor.scala
+++ functional/Visitor.scala
@@ -1,12 +1,22 @@
-case class TextMessage(text:String, sender:String)
-case class MultimediaMessage(attachment:Array[Byte], mime:String, sender:String)
+case class TextMessage(text:String, sender:String, recipient:String, timestamp:Long)
+case class MultimediaMessage(attachment:Array[Byte], mime:String, sender:String, recipient:String, 
+	timestamp:Long)
 
-class MessageHistory(val messages:List[Any])
+class MessageHistory(val messages:List[Any], val owner:String)
 
 object Messages{
-	def size(history:MessageHistory) = history.messages.foldLeft(0){(sum,message) => message match {
-			case tm:TextMessage => sum + tm.text.length
-			case mm:MultimediaMessage => sum + mm.attachment.length
-			case _ => sum
-		}}
+  def size(history:MessageHistory) = history.messages.foldLeft(0){(sum,message) => message match {
+    case tm:TextMessage => sum + tm.text.length
+    case mm:MultimediaMessage => sum + mm.attachment.length
+    case _ => sum
+  }}
+
+  def sentSize(history:MessageHistory) = history.messages.foldLeft(0){
+	(sum,message) => message match {
+      case tm:TextMessage => if (history.owner == tm.sender) sum + tm.text.length else sum
+      case mm:MultimediaMessage => 
+        if (history.owner == mm.sender) sum + mm.attachment.length else sum
+      case _ => sum
+  }}
+
 }
